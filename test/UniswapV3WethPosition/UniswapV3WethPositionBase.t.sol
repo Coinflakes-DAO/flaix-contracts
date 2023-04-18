650e2aa2a3ea47d5a33f4c27e8cfa565369cb6b3
Ned Albo <ned.albo@mailfence.com>
2023-04-18 09:45:45 +0200

    prepared weth tests

--- test/UniswapV3WethPosition/UniswapV3WethPositionBase.t.sol
+++ test/UniswapV3WethPosition/UniswapV3WethPositionBase.t.sol
@@ -12,33 +12,42 @@ interface IChainlinkPriceFeed {
 }
 
 contract UniswapV3WethPositionBaseTest is Base_Test {
-    UniswapV3WethPosition public position;
+    UniswapV3WethPosition public usdcPosition;
+    UniswapV3WethPosition public usdtPosition;
 
     address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address public constant POOL = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
+    address public constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address public constant USDC_POOL = 0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640;
+    address public constant USDT_POOL = 0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36;
     address public constant UNISWAP_POSITION_MANAGER = 0xC36442b4a4522E871399CD717aBDD847Ab11FE88;
     address public constant ETH_PRICE_ORACLE = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
 
     IERC20Metadata public wethToken = IERC20Metadata(WETH);
     IERC20Metadata public usdcToken = IERC20Metadata(USDC);
+    IERC20Metadata public usdtToken = IERC20Metadata(USDT);
 
     IChainlinkPriceFeed priceFeed = IChainlinkPriceFeed(ETH_PRICE_ORACLE);
     int256 public ethPrice;
 
     function setUp() public override {
         super.setUp();
-        setUp_tokens();
-        setUp_Position();
         setUp_ETHPriceOracle();
     }
 
     function setUp_Position() public {
-        position = new UniswapV3WethPosition(
-            "Uniswap V3 Test Position",
-            "UNIV3-TEST-POS",
+        usdcPosition = new UniswapV3WethPosition(
+            "Uniswap V3 Test USDC Position",
+            "UNIV3-TEST-USDC-POS",
             UNISWAP_POSITION_MANAGER,
-            POOL,
+            USDC_POOL,
+            users.bob
+        );
+        usdtPosition = new UniswapV3WethPosition(
+            "Uniswap V3 Test Position",
+            "UNIV3-TEST-USDT-POS",
+            UNISWAP_POSITION_MANAGER,
+            USDT_POOL,
             users.bob
         );
     }
@@ -54,9 +63,22 @@ contract UniswapV3WethPositionBaseTest is Base_Test {
     }
 
     modifier whenUserHasEnoughUsdc(address user, uint256 wethAmount) {
-        uint256 usdcAmount = position.getRequiredAmount0(wethAmount);
+        uint256 usdcAmount = usdcPosition.getRequiredAmount0(wethAmount);
         deal(USDC, user, usdcAmount, true);
         assertEq(usdcToken.balanceOf(user), usdcAmount);
+        _;
+    }
+
+    modifier whenUserHasUsdt(address user, uint256 usdtAmount) {
+        deal(USDT, user, usdtAmount, true);
+        assertEq(usdtToken.balanceOf(user), usdtAmount);
+        _;
+    }
+
+    modifier whenUserHasEnoughUsdt(address user, uint256 wethAmount) {
+        uint256 usdtAmount = usdtPosition.getRequiredAmount0(wethAmount);
+        deal(USDT, user, usdtAmount, true);
+        assertEq(usdtToken.balanceOf(user), usdtAmount);
         _;
     }
 
@@ -66,8 +88,15 @@ contract UniswapV3WethPositionBaseTest is Base_Test {
         _;
     }
 
-    modifier whenUserHasEnoughEth(address user, uint256 usdcAmount) {
-        uint256 ethAmount = position.getRequiredAmount1(usdcAmount);
+    modifier whenUserHasEnoughEthForUsdc(address user, uint256 usdcAmount) {
+        uint256 ethAmount = usdcPosition.getRequiredAmount1(usdcAmount);
+        deal(user, ethAmount);
+        assertEq(user.balance, ethAmount);
+        _;
+    }
+
+    modifier whenUserHasEnoughEthForUsdt(address user, uint256 usdtAmount) {
+        uint256 ethAmount = usdtPosition.getRequiredAmount1(usdtAmount);
         deal(user, ethAmount);
         assertEq(user.balance, ethAmount);
         _;
@@ -75,41 +104,76 @@ contract UniswapV3WethPositionBaseTest is Base_Test {
 
     modifier whenUserHasApprovedWeth(address user) {
         vm.prank(user);
-        wethToken.approve(address(position), 2 ** 256 - 1);
+        wethToken.approve(address(usdcPosition), 2 ** 256 - 1);
+        wethToken.approve(address(usdtPosition), 2 ** 256 - 1);
         _;
     }
 
     modifier whenUserHasApprovedUsdc(address user) {
         vm.prank(user);
-        usdcToken.approve(address(position), 2 ** 256 - 1);
+        usdcToken.approve(address(usdcPosition), 2 ** 256 - 1);
+        _;
+    }
+
+    modifier whenUserHasApprovedUsdt(address user) {
+        vm.prank(user);
+        usdtToken.approve(address(usdtPosition), 2 ** 256 - 1);
         _;
     }
 
     modifier whenToken0IsUsdc() {
-        assertEq(position.pool().token0(), USDC);
+        assertEq(usdcPosition.pool().token0(), USDC);
+        _;
+    }
+
+    modifier whenToken1IsUsdt() {
+        assertEq(usdtPosition.pool().token1(), USDT);
         _;
     }
 
     modifier whenToken1IsWeth() {
-        assertEq(position.pool().token1(), WETH);
+        assertEq(usdcPosition.pool().token1(), WETH);
         _;
     }
 
-    modifier whenUserHasAddedLiquidity(address user, uint256 wethAmount) {
+    modifier whenToken0IsWeth() {
+        assertEq(usdtPosition.pool().token0(), WETH);
+        _;
+    }
+
+    modifier whenUserHasAddedUsdcLiquidity(address user, uint256 wethAmount) {
         deal(WETH, user, wethAmount, true);
-        uint256 usdcAmount = position.getRequiredAmount0(wethAmount);
+        uint256 usdcAmount = usdcPosition.getRequiredAmount0(wethAmount);
         deal(USDC, user, usdcAmount, true);
         vm.startPrank(user);
-        wethToken.approve(address(position), wethAmount);
-        usdcToken.approve(address(position), usdcAmount);
-        position.addLiquidity(wethAmount, usdcAmount, 0, 0, user, block.timestamp);
+        wethToken.approve(address(usdcPosition), wethAmount);
+        usdcToken.approve(address(usdcPosition), usdcAmount);
+        usdcPosition.addLiquidity(wethAmount, usdcAmount, 0, 0, user, block.timestamp);
         vm.stopPrank();
         _;
     }
 
-    modifier whenUserHasApprovedLiquidity(address user) {
+    modifier whenUserHasAddedUsdtLiquidity(address user, uint256 wethAmount) {
+        deal(WETH, user, wethAmount, true);
+        uint256 usdtAmount = usdtPosition.getRequiredAmount1(wethAmount);
+        deal(USDT, user, usdtAmount, true);
+        vm.startPrank(user);
+        wethToken.approve(address(usdtPosition), wethAmount);
+        usdtToken.approve(address(usdtPosition), usdtAmount);
+        usdtPosition.addLiquidity(wethAmount, usdtAmount, 0, 0, user, block.timestamp);
+        vm.stopPrank();
+        _;
+    }
+
+    modifier whenUserHasApprovedUsdcLiquidity(address user) {
         vm.prank(user);
-        position.approve(address(position), 2 ** 256 - 1);
+        usdcPosition.approve(address(usdcPosition), 2 ** 256 - 1);
+        _;
+    }
+
+    modifier whenUserHasApprovedUsdtLiquidity(address user) {
+        vm.prank(user);
+        usdtPosition.approve(address(usdtPosition), 2 ** 256 - 1);
         _;
     }
 
---
+++ test/UniswapV3WethPosition/addLiquidity/addLiquidity.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@test/UniswapV3WethPosition/UniswapV3WethPositionBase.t.sol";
+import "@src/uniswapv3/UniswapV3WethPosition.sol";
+
+contract AddLiquidityTest is UniswapV3WethPositionBaseTest {}
